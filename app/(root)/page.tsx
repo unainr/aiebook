1b95382e463d6854930852839efe283c8387072d
UNAINR <munain605@gmail.com>
2025-10-18 11:54:41 +0500

    add sanity

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { HomeView } from '@/modules/home/ui/views/home-view'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+     <HomeView/> 
+    </>
+  )
+}
+
+export default Home
---
+++ modules/home/ui/views/home-view.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const HomeView = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen'>HomeView</div>
+  )
+}
