f1b0c1df599f61d9db8190c9c5b9c9e85437f9b1
UNAINR <munain605@gmail.com>
2025-10-18 23:15:17 +0500

    add the sanity write client

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { eBook } from './ebook'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [eBook],
 }
--- sanity/structure.ts
+++ sanity/structure.ts
@@ -4,4 +4,4 @@ import type {StructureResolver} from 'sanity/structure'
 export const structure: StructureResolver = (S) =>
   S.list()
     .title('Content')
-    .items(S.documentTypeListItems())
+    .items([ S.documentTypeListItem("eBook").title("E-Book"),])
